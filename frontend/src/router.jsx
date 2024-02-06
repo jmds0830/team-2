38c719f0a2e34bf7147771e7ae9310ceec6693e4
JM <jmdsegismundo@gmail.com>
2024-02-06 17:38:09 +0800

    feat: created student registration page

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
+import StudentRegistrationPage from './pages/StudentRegistrationPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/api/register',
+    element: <StudentRegistrationPage />,
   },
   {
     path: '*',
