15e634e2ede58d23af88b5d4947b7230a6ae9d4f
JM <jmdsegismundo@gmail.com>
2024-02-08 19:29:50 +0800

    feat: added student-info page

--- backend/models/StudentRegisterModel.js
+++ backend/models/StudentRegisterModel.js
@@ -4,8 +4,14 @@ const studentSchema = new Schema({
   firstName: String,
   lastName: String,
   course: String,
+  college: String,
   email: String,
   username: String,
+  studentId: Number,
+  image: {
+    data: Buffer,
+    contentType: String,
+  },
 });
 
 const Student = model('Student', studentSchema);
---
+++ frontend/src/pages/StudentInfoPage.jsx
@@ -0,0 +1,87 @@
+import { useState, useEffect } from 'react';
+import { Grid, Image, Button } from '@mantine/core';
+import { useParams } from 'react-router-dom';
+import EnSysBanner from '../components/enSysBanner';
+import styles from '../styles/StudentInfo.module.css';
+
+function StudentInfoPage() {
+  const [studentData, setStudentData] = useState([]);
+  const { id } = useParams();
+
+  async function fetchStudentData() {
+    try {
+      if (!id) return;
+      const response = await fetch(`http://localhost:3000/student-info/${id}`);
+      const data = await response.json();
+      setStudentData(data);
+    } catch (error) {
+      console.error('Error fetching student info:', error.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchStudentData();
+  }, [id]);
+
+  return (
+    <>
+      <EnSysBanner />
+      <div>
+        {studentData?.student?.map((student, index) => (
+          <div className={styles.studentInfoContainer} key={index}>
+            <div className={styles.title}>
+              <h2>Student Information</h2>
+              <Image
+                radius="md"
+                src={null}
+                h={150}
+                w={150}
+                fallbackSrc="https://placehold.co/600x400?text=Student Photo"
+              />
+            </div>
+            <Grid className={styles.grid} cols={2}>
+              <Grid.Col className={styles.grid} span={5.5}>
+                <span>First Name: </span>
+                <span className={styles.info}>{student.firstName}</span>
+              </Grid.Col>
+              <Grid.Col className={styles.grid} span={5.5}>
+                <span>Last Name: </span>
+                <span className={styles.info}>{student.lastName}</span>
+              </Grid.Col>
+              <Grid.Col className={styles.grid} span={5.5}>
+                <span> Course: </span>
+                <span className={styles.info}>{student.course}</span>
+              </Grid.Col>
+              <Grid.Col className={styles.grid} span={5.5}>
+                <span>College: </span>
+                <span className={styles.info}>{student.college}</span>
+              </Grid.Col>
+              <Grid.Col className={styles.grid} span={5.5}>
+                <span>Student Number: </span>
+                <span className={styles.info}>{student.studentId}</span>
+              </Grid.Col>
+              <Grid.Col className={styles.grid} span={5.5}>
+                <span>Email: </span>
+                <span className={styles.info}>{student.email}</span>
+              </Grid.Col>
+              <Grid.Col className={styles.grid} span={5.5}>
+                <span>Username: </span>
+                <span className={styles.info}>{student.username}</span>
+              </Grid.Col>
+              <Grid.Col className={styles.grid} span={5.5}>
+                <span>Password: </span> <span className={styles.info}></span>
+              </Grid.Col>
+            </Grid>
+            <div className={styles.buttonContainer}>
+              <Button variant="filled" color="gray">
+                Edit Information
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default StudentInfoPage;
